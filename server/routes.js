b24835bdb6eb776fac6703c12bc3ad76d526297f
feycot <feycot@gmail.com>
2021-05-18 14:12:05 +0500

    add rollbar

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,11 @@ const _ = require('lodash');
 
 module.exports = (app) => {
   app.get('/', { name: 'root' }, (req, reply) => {
-    const serverIp = _.get(process.env, 'SERVER_IP', 'не определен');
-    reply.view('index', { serverIp });
+    const serverMessage = process.env.SERVER_MESSAGE;
+
+    reply.view('index', { serverMessage });
+  });
+  app.get('/error', {name: 'error'}, (_req, _reply) => {
+    throw new Error('Oops! Something went wrong!');
   });
 };
